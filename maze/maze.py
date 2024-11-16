aee7c9c890e59d420cd58fb74b3f2791aa6d6515
amir-razmi <amir.dv8212@gmail.com>
2024-11-16 12:38:29 +0330

    complete maze

--- maze/maze.py
+++ maze/maze.py
@@ -2,7 +2,7 @@ import pygame
 import random
 
 width, height = 1200,900
-tile = 10
+tile = 100
 cols,rows = width // tile, height // tile 
 wall_color = pygame.Color('darkorange')
 screen_color = pygame.Color('darkslategray')
@@ -20,14 +20,18 @@ class Cell:
     self.col = x
     self.row = y
     self.visited = False
-    x,y = x * tile, y * tile
-    self.walls = {
+    self.solver_visited = False
+    self.walls = self.generate_walls()
+    self.solver_walls = self.generate_walls()
+
+  def generate_walls(self):
+    x,y = self.col * tile, self.row * tile
+    return {
       "top": {"s": (x, y), "e": (x + tile , y )},
       "right": {"s": (x + tile, y), "e": (x + tile, y + tile)},
       "bottom": {"s": (x, y + tile), "e": (x + tile, y + tile)},
       "left": {"s": (x, y), "e": (x, y + tile)}
     }
-
 
   def draw_current_cell(self):
     x,y = self.col * tile, self.row * tile
@@ -42,33 +46,27 @@ class Cell:
       if wall != None:
         pygame.draw.line(screen , wall_color, wall["s"], wall["e"])
 
+  def is_neibor_cell_exists(self, x, y):
+    return y < rows and y >= 0 and x < cols and x >= 0
+
   def get_neibor_cell(self, x, y):
-    if self.col == 0 and self.row == 0:
-      print(x,y)
-    if y < rows and y >= 0 and x < cols and x >= 0:
+    if self.is_neibor_cell_exists(x, y):
       cell = cells[x * rows + y]
-      if not cell.visited:
-        return cell
+      return cell
+  def get_all_neibor_cells(self):
+    return {
+      "top": self.get_neibor_cell(self.col , self.row - 1),
+      "bottom": self.get_neibor_cell(self.col, self.row + 1),
+      "right": self.get_neibor_cell(self.col + 1, self.row),
+      "left": self.get_neibor_cell(self.col - 1, self.row)
+    }
 
   def select_next_cell(self):
-    top = self.get_neibor_cell(self.col , self.row + 1)
-    bottom = self.get_neibor_cell(self.col, self.row - 1)
-    right = self.get_neibor_cell(self.col + 1, self.row)
-    left = self.get_neibor_cell(self.col - 1, self.row)
-
-    all_neibor_coords = []
-    if top != None:
-      all_neibor_coords.append(top)
-    if bottom != None:
-      all_neibor_coords.append(bottom)
-    if right != None:
-      all_neibor_coords.append(right)
-    if left != None:
-      all_neibor_coords.append(left)
-    if self.col == 0 and self.row == 0:
-      print(all_neibor_coords)
-    if len(all_neibor_coords) > 0:
-      next_cell = random.choice(all_neibor_coords)
+    all_neibors = self.get_all_neibor_cells()
+    not_visited_neibors = [neibor for neibor in all_neibors.values() if neibor and not neibor.visited]
+    
+    if len(not_visited_neibors) > 0:
+      next_cell = random.choice(not_visited_neibors)
       self.remove_walls(next_cell)
       next_cell.visited = True
       return next_cell
@@ -81,18 +79,21 @@ class Cell:
           pygame.draw.line(screen , screen_color, next_cell.walls[nw]["s"], next_cell.walls[nw]["e"])
           next_cell.walls[nw] = None 
           self.walls[cw] = None
+          next_cell.solver_walls[nw] = None 
+          self.solver_walls[cw] = None
 
 cells = [Cell(col , row) for col in range(cols) for row in range(rows)]
 
 
 stacks = [[cells[0]]]
 cells[0].visited = True
-for _ in range(int(len(cells) / 500)):
-  not_visited_cells = [cell for cell in cells if not cell.visited]
-  if len(not_visited_cells) > 0:
-    random_cell = random.choice(not_visited_cells)
-    random_cell.visited = True
-    stacks.append([random_cell])
+# for _ in range(int(len(cells) / 500)):
+# for _ in range(1):
+#   not_visited_cells = [cell for cell in cells if not cell.visited]
+#   if len(not_visited_cells) > 0:
+#     random_cell = random.choice(not_visited_cells)
+#     random_cell.visited = True
+#     stacks.append([random_cell])
 
 
 while running:
@@ -118,6 +119,61 @@ while running:
 
   clock.tick(60)
   pygame.display.flip()
+
+  empty_stacks = [s for s in stacks if len(s) <= 0]
+  if len(empty_stacks) == len(stacks):
+    break
+
+stack = [cells[0]]
+cells[0].solver_visited = True
+final_cell = cells[-1]
+
+while running:
+  screen.fill(screen_color)
+  for event in pygame.event.get():
+    running = event.type != pygame.QUIT
+
+  for c in cells:
+    c.draw()
+
+  if len(stack) <= 0:
+    continue
+
+  current_cell = stack.pop()
+  current_cell.solver_visited = True
+  current_cell.draw_current_cell()
+
+  clock.tick(3)
+  pygame.display.flip()
+
+  if current_cell == final_cell:
+    stack.append(current_cell)
+    continue
+
+
+  neibor_cells = current_cell.get_all_neibor_cells()
+
+  possible_neibors = []
+  if neibor_cells["top"] and current_cell.solver_walls["top"] == None and neibor_cells["top"].solver_walls["bottom"] == None:
+    possible_neibors.append(neibor_cells["top"])
+  if neibor_cells["right"] and current_cell.solver_walls["right"] == None and neibor_cells["right"].solver_walls["left"] == None:
+    possible_neibors.append(neibor_cells["right"])
+  if neibor_cells["bottom"] and current_cell.solver_walls["bottom"] == None and neibor_cells["bottom"].solver_walls["top"] == None:
+    possible_neibors.append(neibor_cells["bottom"])
+  if neibor_cells["left"] and current_cell.solver_walls["left"] == None and neibor_cells["left"].solver_walls["right"] == None:
+    possible_neibors.append(neibor_cells["left"])
+
+
+  x = [n for n in possible_neibors if not n.solver_visited]
+  for i in possible_neibors:
+    print("Currnet Cell : " , (current_cell.col, current_cell.row) , " ==> " , (i.col,i.row))
+  if len(x) > 0:
+    next_cell = random.choice(x)
+    stack.append(current_cell)
+    stack.append(next_cell)
+  else:
+    current_cell.solver_walls = current_cell.generate_walls()
+
 
 
 
