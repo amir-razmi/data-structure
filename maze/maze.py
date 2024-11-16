e3363110439b419d2cd8c1da319f14623ee694b2
amir-razmi <amir.dv8212@gmail.com>
2024-11-16 20:55:26 +0330

    clean maze codes

--- maze/maze.py
+++ maze/maze.py
@@ -6,6 +6,9 @@ tile = 30
 cols,rows = width // tile, height // tile 
 wall_color = pygame.Color('darkorange')
 screen_color = pygame.Color('darkslategray')
+current_cell_color = pygame.Color("saddlebrown")
+black = pygame.Color("black")
+white = pygame.Color("white")
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
@@ -15,9 +18,9 @@ class Cell:
   def __init__(self, x , y):
     self.col = x
     self.row = y
-    self.visited = False
-    self.solver_visited = False
+    self.generator_visited = False
     self.walls = self.generate_walls()
+    self.solver_visited = False
     self.solver_walls = self.generate_walls()
 
   def generate_walls(self):
@@ -31,26 +34,26 @@ class Cell:
 
   def draw_as_current_cell(self):
     x,y = self.col * tile, self.row * tile
-    pygame.draw.rect(screen, pygame.Color("saddlebrown"), (x + 2, y + 2 , tile - 2, tile - 2))
+    pygame.draw.rect(screen, current_cell_color, (x + 2, y + 2 , tile - 2, tile - 2))
 
   def draw(self):
     x,y = self.col * tile, self.row * tile
     if self.solver_visited:
-      pygame.draw.rect(screen, pygame.Color("black"), (x, y, tile, tile))
-      pygame.draw.rect(screen, pygame.Color("white"), pygame.Rect(x + tile /4, y + tile /4 , tile / 2 , tile /2 ))
-    elif self.visited:
-      pygame.draw.rect(screen, pygame.Color("black"), (x, y, tile, tile))
+      pygame.draw.rect(screen, black, (x, y, tile, tile))
+      pygame.draw.rect(screen, white, pygame.Rect(x + tile/4, y + tile/4 , tile/2 , tile/2 ))
+    elif self.generator_visited:
+      pygame.draw.rect(screen, black, (x, y, tile, tile))
 
     for wall in self.walls.values():
       if wall != None:
         pygame.draw.line(screen , wall_color, wall["s"], wall["e"])
 
-  def get_all_neibor_cells(self):
+  def get_all_neighbor_cells(self):
     return {
-      "top": maze.get_neibor_cell(self.col , self.row - 1),
-      "bottom": maze.get_neibor_cell(self.col, self.row + 1),
-      "right": maze.get_neibor_cell(self.col + 1, self.row),
-      "left": maze.get_neibor_cell(self.col - 1, self.row)
+      "top": maze.get_cell_by_coords(self.col , self.row - 1),
+      "bottom": maze.get_cell_by_coords(self.col, self.row + 1),
+      "right": maze.get_cell_by_coords(self.col + 1, self.row),
+      "left": maze.get_cell_by_coords(self.col - 1, self.row)
     }
 
 
@@ -61,60 +64,59 @@ class Maze:
     self.stacks = [[self.cells[0]]]
     self.doubled_rows = []
 
-  def select_next_cell(self, current_cell):
-    all_neibors = current_cell.get_all_neibor_cells()
-    not_visited_neibors = [n for n in all_neibors.values() if n and not n.visited]
+  def select_next_cell(self, cur_cell):
+    all_neighbors = cur_cell.get_all_neighbor_cells()
+    unvisited_neighbors = [n for n in all_neighbors.values() if n and not n.generator_visited]
 
-    if len(not_visited_neibors) > 0:
-      next_cell = random.choice(not_visited_neibors)
-      self.remove_walls(current_cell, next_cell)
-      next_cell.visited = True
+    if len(unvisited_neighbors) > 0:
+      next_cell = random.choice(unvisited_neighbors)
+      self.remove_walls(cur_cell, next_cell)
+      next_cell.generator_visited = True
       return next_cell
 
-  def remove_walls(self, cell1 , cell2):
-    for nw in cell2.walls:
-      for cw in cell1.walls:
-        if cell2.walls[nw] != None and cell2.walls[nw] == cell1.walls[cw]:
-          pygame.draw.line(screen , screen_color, cell2.walls[nw]["s"], cell2.walls[nw]["e"])
-          cell2.walls[nw] = None 
-          cell1.walls[cw] = None
-          cell2.solver_walls[nw] = None 
-          cell1.solver_walls[cw] = None
+  def remove_walls(self, cell_1 , cell_2):
+    for nw in cell_2.walls:
+      for cw in cell_1.walls:
+        if cell_2.walls[nw] != None and cell_2.walls[nw] == cell_1.walls[cw]:
+          pygame.draw.line(screen , screen_color, cell_2.walls[nw]["s"], cell_2.walls[nw]["e"])
+          cell_2.walls[nw] = None 
+          cell_1.walls[cw] = None
+          cell_2.solver_walls[nw] = None 
+          cell_1.solver_walls[cw] = None
 
   def draw_cells(self):
     for c in self.cells:
       c.draw()
 
-  def move(self):
+  def move_generators(self):
     for stack in self.stacks:
       if len(stack) <= 0:
         continue
 
-      current_cell = stack.pop()
-      current_cell.visited = True
-      current_cell.draw_as_current_cell()
+      cur_cell = stack.pop()
+      cur_cell.generator_visited = True
+      cur_cell.draw_as_current_cell()
 
-      next_cell = self.select_next_cell(current_cell)
+      next_cell = self.select_next_cell(cur_cell)
       if next_cell:
-        stack.append(current_cell)
+        stack.append(cur_cell)
         stack.append(next_cell)
-      if self.hard_mode and current_cell.row % 2 == 0 and current_cell.row not in self.doubled_rows:
-        another_cell = self.select_next_cell(current_cell)
+      if self.hard_mode and cur_cell.row % 2 == 0 and cur_cell.row not in self.doubled_rows:
+        another_cell = self.select_next_cell(cur_cell)
         if another_cell:
-          self.doubled_rows.append(current_cell.row)
+          self.doubled_rows.append(cur_cell.row)
           self.stacks.append([another_cell])
 
-  def is_maze_complete(self):
+  def is_maze_created(self):
     empty_stacks = [s for s in self.stacks if len(s) <= 0]
     return len(empty_stacks) == len(self.stacks)
   
-  def is_neibor_cell_exists(self, x, y):
+  def is_cell_exists(self, x, y):
     return y < rows and y >= 0 and x < cols and x >= 0
 
-  def get_neibor_cell(self, x, y):
-    if self.is_neibor_cell_exists(x, y):
-      cell = self.cells[x * rows + y]
-      return cell
+  def get_cell_by_coords(self, x, y):
+    if self.is_cell_exists(x, y):
+      return self.cells[x * rows + y]
 
 class MazeSolver:
   def __init__(self):
@@ -122,42 +124,42 @@ class MazeSolver:
     self.stack = [self.cells[0]]
     self.goal = self.cells[-1]
 
-  def move(self):
+  def move_solver(self):
     if len(self.stack) <= 0:
       return
 
-    current_cell = self.stack.pop()
-    current_cell.solver_visited = True
-    current_cell.draw_as_current_cell()
+    cur_cell = self.stack.pop()
+    cur_cell.solver_visited = True
+    cur_cell.draw_as_current_cell()
 
-    if current_cell == self.goal:
-      self.stack.append(current_cell)
+    if cur_cell == self.goal:
+      self.stack.append(cur_cell)
       return
 
-    next_cell = self.select_next_cell(current_cell)
+    next_cell = self.select_next_cell(cur_cell)
     if next_cell:
-      self.stack.append(current_cell)
+      self.stack.append(cur_cell)
       self.stack.append(next_cell)
     else:
-      current_cell.solver_visited = False
-      current_cell.solver_walls = current_cell.generate_walls()
+      cur_cell.solver_visited = False
+      cur_cell.solver_walls = cur_cell.generate_walls()
 
   def select_next_cell(self, cur_cell):
-    neibor_cells = cur_cell.get_all_neibor_cells()
-    possible_neibors = []
-    if neibor_cells["top"] and cur_cell.solver_walls["top"] == None and neibor_cells["top"].solver_walls["bottom"] == None:
-      possible_neibors.append(neibor_cells["top"])
-    if neibor_cells["right"] and cur_cell.solver_walls["right"] == None and neibor_cells["right"].solver_walls["left"] == None:
-      possible_neibors.append(neibor_cells["right"])
-    if neibor_cells["bottom"] and cur_cell.solver_walls["bottom"] == None and neibor_cells["bottom"].solver_walls["top"] == None:
-      possible_neibors.append(neibor_cells["bottom"])
-    if neibor_cells["left"] and cur_cell.solver_walls["left"] == None and neibor_cells["left"].solver_walls["right"] == None:
-      possible_neibors.append(neibor_cells["left"])
+    neighbor_cells = cur_cell.get_all_neighbor_cells()
+    possible_neighbors = []
+    if neighbor_cells["top"] and cur_cell.solver_walls["top"] == None and neighbor_cells["top"].solver_walls["bottom"] == None:
+      possible_neighbors.append(neighbor_cells["top"])
+    if neighbor_cells["right"] and cur_cell.solver_walls["right"] == None and neighbor_cells["right"].solver_walls["left"] == None:
+      possible_neighbors.append(neighbor_cells["right"])
+    if neighbor_cells["bottom"] and cur_cell.solver_walls["bottom"] == None and neighbor_cells["bottom"].solver_walls["top"] == None:
+      possible_neighbors.append(neighbor_cells["bottom"])
+    if neighbor_cells["left"] and cur_cell.solver_walls["left"] == None and neighbor_cells["left"].solver_walls["right"] == None:
+      possible_neighbors.append(neighbor_cells["left"])
 
-    unvisited_neibors = [n for n in possible_neibors if not n.solver_visited]
+    unvisited_neighbors = [n for n in possible_neighbors if not n.solver_visited]
     closest_cell_to_goal = None
-    if len(unvisited_neibors) > 0:
-      for cell in unvisited_neibors:
+    if len(unvisited_neighbors) > 0:
+      for cell in unvisited_neighbors:
         if closest_cell_to_goal == None:
           closest_cell_to_goal = cell
         elif (
@@ -179,12 +181,12 @@ while generating_maze:
     generating_maze = event.type != pygame.QUIT
 
   maze.draw_cells()
-  maze.move()
+  maze.move_generators()
 
   clock.tick(60)
   pygame.display.flip()
 
-  generating_maze = not maze.is_maze_complete()
+  generating_maze = not maze.is_maze_created()
 
 solving_maze = True
 while solving_maze:
@@ -193,7 +195,7 @@ while solving_maze:
     solving_maze = event.type != pygame.QUIT
 
   maze.draw_cells()
-  maze_solver.move()
+  maze_solver.move_solver()
 
   clock.tick(60)
   pygame.display.flip()
